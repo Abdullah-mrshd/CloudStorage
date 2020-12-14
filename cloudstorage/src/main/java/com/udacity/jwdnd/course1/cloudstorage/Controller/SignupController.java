4294da70e85e1aafb874e9dd5f8e8573ea7e2bca
Abdullah Almurshed <abdullahmrshd60@gmail.com>
2020-12-14 18:00:58 +0300

    added custom error page

---
+++ cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/Controller/SignupController.java
@@ -0,0 +1,55 @@
+package com.udacity.jwdnd.course1.cloudstorage.Controller;
+
+import com.udacity.jwdnd.course1.cloudstorage.Model.User;
+import com.udacity.jwdnd.course1.cloudstorage.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/signup")
+public class SignupController
+{
+    @Autowired
+    UserService userService;
+
+    @GetMapping
+    public String getPage()
+    {
+        return "signup";
+    }
+
+    @PostMapping
+    public String postPage(User user, Model model)
+    {
+        String signupError = null;
+        //Check if username available
+        if(!userService.isUsernameAvailable(user.getUsername()))
+        {
+            signupError = "The username already exists.";
+        }
+
+        if(signupError == null)
+        {
+            int rowsAdded = userService.createUser(user);
+            if(rowsAdded < 0)
+            {
+                signupError = "There was and error signing you up. Please try again.";
+            }
+        }
+        if(signupError == null)
+        {
+            model.addAttribute("signupSuccess", true);
+        }
+        else
+        {
+            model.addAttribute("signupError", signupError);
+        }
+        return "signup";
+    }
+
+
+}
